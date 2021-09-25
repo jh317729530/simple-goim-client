1d7c1cc67801c22ecb16fb52dc785ffc79eb4107
ganjunhui <317729530@qq.com>
2021-09-25 23:23:57 +0800

    log

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"math/rand"
+	"os"
 	"simple-goim-client/internal/conf"
+	"time"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	flag.Parse()
-	if err := conf.Init(); err != nil {
+	rand.Seed(time.Now().UnixNano())
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	file, err := os.OpenFile("simple-goim-client.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Err(err)
 		panic(err)
 	}
 
-	fmt.Println(conf.Conf.HttpServer.Addr)
+	subLogger := log.Logger.Output(io.MultiWriter(os.Stdout, file))
+	log.Logger = subLogger
+
+	log.Info().Msg("test")
+
+	if err := conf.Init(); err != nil {
+		log.Error().Msg("Global config init failed")
+		panic(err)
+
+	}
+
 }
--- go.mod
+++ go.mod
@@ -10,9 +10,10 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/rs/zerolog v1.25.0
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20210923061019-b8560ed6a9b7 // indirect
+	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
